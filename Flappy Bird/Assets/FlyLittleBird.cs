0d89e55fe691174c09a11a544fab0d78cab616e8
Priyam Jain <priyam.j@somaiya.edu>
2020-03-31 20:47:52 +0530

    First commit

---
+++ Flappy Bird/Assets/FlyLittleBird.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyLittleBird : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameManager gameManager;
+    public float velocity = 4;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            //jump
+            rb.velocity = Vector2.up * velocity;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        gameManager.GameOver();
+    }
+}
